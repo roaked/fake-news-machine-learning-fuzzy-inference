05944201c116031e1838e412a95a5d532f6ab2fa
roaked <ossunarola@gmail.com>
2023-11-18 18:46:09 +0100

    stats

--- FMID5-docs/Statistics.m
+++
@@ -1,6 +0,0 @@
-%Statistics
-avgNumbToken = sum(metaInputs(1,:))/7204;
-avgNumbTokenTrue = sum(metaInputs(1,[1:3602]))/3602;
-avgNumbTokenFalse = sum(metaInputs(1,[3603:end]))/3602;
-
-%Dá igual às stats que estão no paper
